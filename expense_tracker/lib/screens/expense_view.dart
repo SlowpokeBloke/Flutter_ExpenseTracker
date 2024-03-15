e448d159ea0141157cfecbae094ee45af1ac0e68
Abenezer <abitysank@gmail.com>
2024-03-15 17:43:15 -0400

    Update expense_view.dart
    Transform ExpenseView to StatefulWidget and dynamically load expenses from database.

--- expense_tracker/lib/screens/expense_view.dart
+++ expense_tracker/lib/screens/expense_view.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/helpers/database_helper.dart';
 
-class ExpenseView extends StatelessWidget {
+class ExpenseView extends StatefulWidget {
+  @override
+  _ExpenseViewState createState() => _ExpenseViewState();
+}
+
+class _ExpenseViewState extends State<ExpenseView> {
+  List<Map<String, dynamic>> _expenses = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadExpenses();
+  }
+
+  Future<void> _loadExpenses() async {
+    // Assuming you have a method getExpenses() that returns a list of expenses
+    _expenses = await DatabaseHelper().getExpenses();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense View'), 
+        title: Text('Expense View'),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         centerTitle: true,
         elevation: 2,
       ),
-      
-
-  
+      body: ListView.builder(
+        itemCount: _expenses.length,
+        itemBuilder: (context, index) {
+          final expense = _expenses[index];
+          return ListTile(
+            leading: Icon(Icons.monetization_on), // or any other relevant icon
+            title: Text('${expense['categoryName']}: \$${expense['amount']}'),
+            subtitle:
+                Text(expense['description']), // Assuming there's a description
+            trailing: Text(
+                expense['date']), // Assuming you store the date as a string
+          );
+        },
+      ),
     );
   }
 }
