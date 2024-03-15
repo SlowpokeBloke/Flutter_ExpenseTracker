a53de3148b8b287b73760165416316bf90f7eb11
Abenezer <abitysank@gmail.com>
2024-03-15 17:45:29 -0400

    Update expense_view.dart
    ExpenseView to asynchronously load and display expenses with a dedicated DatabaseHelper instance

--- expense_tracker/lib/screens/expense_view.dart
+++ expense_tracker/lib/screens/expense_view.dart
@@ -2,49 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/helpers/database_helper.dart';
 
 class ExpenseView extends StatefulWidget {
-  @override
-  _ExpenseViewState createState() => _ExpenseViewState();
+ @override
+ _ExpenseViewState createState() => _ExpenseViewState();
 }
 
 class _ExpenseViewState extends State<ExpenseView> {
-  List<Map<String, dynamic>> _expenses = [];
+ List<Map<String, dynamic>> _expenses = [];
 
-  @override
-  void initState() {
-    super.initState();
-    _loadExpenses();
-  }
+ @override
+ void initState() {
+   super.initState();
+   _loadExpenses();
+ }
 
-  Future<void> _loadExpenses() async {
-    // Assuming you have a method getExpenses() that returns a list of expenses
-    _expenses = await DatabaseHelper().getExpenses();
-    setState(() {});
-  }
+Future<void> _loadExpenses() async {
+ // Use DatabaseHelper instance to access the database
+ final dbHelper = DatabaseHelper();
+ _expenses = await dbHelper.getExpenses();
+ setState(() {});
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Expense View'),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        centerTitle: true,
-        elevation: 2,
-      ),
-      body: ListView.builder(
-        itemCount: _expenses.length,
-        itemBuilder: (context, index) {
-          final expense = _expenses[index];
-          return ListTile(
-            leading: Icon(Icons.monetization_on), // or any other relevant icon
-            title: Text('${expense['categoryName']}: \$${expense['amount']}'),
-            subtitle:
-                Text(expense['description']), // Assuming there's a description
-            trailing: Text(
-                expense['date']), // Assuming you store the date as a string
-          );
-        },
-      ),
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Expense View'),
+       backgroundColor: Colors.white,
+       foregroundColor: Colors.black,
+       centerTitle: true,
+       elevation: 2,
+     ),
+     body: ListView.builder(
+       itemCount: _expenses.length,
+       itemBuilder: (context, index) {
+         final expense = _expenses[index];
+         return ListTile(
+           leading: Icon(Icons.monetization_on),
+           title: Text('${expense['categoryName']}: \$${expense['amount']}'),
+           subtitle: Text(expense['description']),
+           trailing: Text(expense['date']),
+         );
+       },
+     ),
+   );
+ }
 }
